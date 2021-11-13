34f175bda153e1988d5ccdcfaab96785ab58d261
shouryacool <84572733+shouryacool@users.noreply.github.com>
2021-11-13 19:37:25 +0530

    Add files via upload

---
+++ 01_Functions.py
@@ -0,0 +1,8 @@
+def percent(marks):
+    return((marks[0]+marks[1]+marks[2]+marks[3]+marks[4])/400)*100
+marks=[45,78,86,77]
+Percentage1=(sum(marks)/400)*100 
+
+marks2=[45,88,92,70]
+Percentage2=(sum(marks2)/400)*100
+print(Percentage1,Percentage2)
